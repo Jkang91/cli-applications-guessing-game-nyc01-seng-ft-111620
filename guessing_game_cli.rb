2e05744e986d456ded175167a7abb9a27c3ea92f
June Young Kang <jkang34@gmail.com>
2020-11-02 20:38:31 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,7 @@
 def run_guessing_game
   puts "Guess between 1 and 6"
   input = gets() 
-  rand_num = rand(1...6)
+  rand_num = rand(1..6)
   while input != "exit" 
     if rand_num == input 
       return "You guessed the correct number!"
